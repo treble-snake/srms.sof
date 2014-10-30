422ec97724e984f5c7a61404f58fa8ef4715ed6f
treble-snake <treble.snake@yandex.ru>
2014-10-30 19:39:36 +0300

    Perks added
    Visual improvement

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ angular.module('MyModule', [])
     .controller('ClassTree', function ($scope, $http) {
 
         $scope.hello = "Loading..";
-        $scope.currentClass = null;
+        $scope.currentClass;
+        $scope.currentStats;
 
         $http.get("data/data.json")
             .success(function (json) {
@@ -14,10 +15,54 @@ angular.module('MyModule', [])
                 console.error(data + ": " + status);
             });
 
-        $scope.choose = function (classId, classData) {
+        $scope.setClass = function (classId, classData) {
             var data = classData;
             data.id = classId;
             $scope.currentClass = data;
+            $scope.currentStats = $scope.calculateStats(classId);
+        };
+
+        $scope.isSelected = function (classId) {
+            return $scope.currentClass !== undefined &&
+                (classId === $scope.currentClass.id || $scope.isAncestor(classId, $scope.currentClass));
+        };
+
+        $scope.addPerk = function (id) {
+            var perk = $scope.getPerk(id);
+            if(perk === undefined)
+                return;
+
+            $scope.applyPerk(perk);
+            perk.selected = !perk.selected;
+        };
+
+        /////////////////////
+
+        $scope.applyPerk = function (perk) {
+            for(id in perk.effects) {
+                $scope.applyPerkEffect(id, perk.effects[id], perk.selected)
+            }
+        };
+
+        $scope.applyPerkEffect = function (id, stats, revert) {
+            for(statId in stats) {
+                $scope.effectsMap[id](statId, stats[statId], revert);
+            }
+        };
+
+        $scope.effectsMap = {
+            add: function(stat, value, revert) {
+                if($scope.currentStats[stat] !== undefined)
+                    $scope.currentStats[stat] += revert ? -1*value : value;
+            }
+        };
+
+        $scope.isAncestor = function (ancestorId, child) {
+            if (ancestorId === undefined || child === undefined)
+                return false;
+            if (child.parent === ancestorId)
+                return true;
+            return $scope.isAncestor(ancestorId, $scope.getClass(child.parent));
         };
 
         $scope.calculateStats = function (classId) {
@@ -28,10 +73,9 @@ angular.module('MyModule', [])
             return $scope.recursiveCalculateStats(classId, {});
         };
 
-        $scope.recursiveCalculateStats = function(id, result) {
-
+        $scope.recursiveCalculateStats = function (id, result) {
             var clazz = $scope.getClass(id);
-            if(clazz === undefined)
+            if (clazz === undefined)
                 return result;
 
             var stats = clazz.stats.base;
@@ -45,10 +89,18 @@ angular.module('MyModule', [])
                     result[key] = stats[key];
             }
             return $scope.recursiveCalculateStats(clazz.parent, result);
-        }
+        };
 
-        $scope.getClass = function(id) {
-            var result = jsonPath($scope.data, "$.classes.." + id);
+        $scope.getClass = function (id) {
+            return $scope.getJsonElement("classes", id);
+        };
+
+        $scope.getPerk = function (id) {
+            return $scope.getJsonElement("perks", id);
+        };
+
+        $scope.getJsonElement = function (container, id) {
+            var result = jsonPath($scope.data[container], "$.." + id);
             return  result === undefined || result.length == 0 ? undefined : result[0];
         }
     });
