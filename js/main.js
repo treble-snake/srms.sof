fd4892aa0cddc6b136f04c48ea4b672583f498d6
Treble Snake <treble.snake@yandex.ru>
2014-11-01 01:37:36 +0300

    Handling sub-stats. Handling effects.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
 var sofApp = angular.module('MyModule', []);
 
 sofApp
-    .directive('statsPane', function () {
-    return {
-        templateUrl: 'app/partials/stats.pane.html',
-        restrict: 'A',
-        scope: {
-            currentStats: '=',
-            stats: '=allStats'
+    .directive('statsPane', function (RecursionHelper) {
+        return {
+            templateUrl: 'app/partials/stats.pane.html',
+            restrict: 'A',
+            scope: {
+                currentStats: '=',
+                stats: '=allStats'
+            },
+            compile: function (element) {
+                // Use the compile function from the RecursionHelper,
+                // And return the linking function(s) which it returns
+                return RecursionHelper.compile(element);
+            }
         }
-    }
-})
+    })
     .controller('ClassTree', function ($scope, $http) {
 
+        /* Public section */
         $scope.hello = "Loading..";
         $scope.currentClass;
         $scope.currentStats;
 
+        // load data
         $http.get("data/data.json")
             .success(function (json) {
                 $scope.data = json;
                 $scope.hello = "Ready!";
+                $scope.setClass('base', json.classes['base']);
             })
             .error(function (data, status) {
                 $scope.hello = "Failed =(";
@@ -31,88 +39,202 @@ sofApp
             var data = classData;
             data.id = classId;
             $scope.currentClass = data;
-            $scope.currentStats = $scope.calculateStats(classId);
+            $scope.currentStats = calculateStats(classId);
         };
 
-        $scope.isSelected = function (classId) {
+        $scope.isClassSelected = function (classId) {
             return $scope.currentClass !== undefined &&
-                (classId === $scope.currentClass.id || $scope.isAncestor(classId, $scope.currentClass));
+                (classId === $scope.currentClass.id || isAncestor(classId, $scope.currentClass));
+        };
+
+        $scope.perkAvailable = function (perk) {
+            var current = $scope.currentClass;
+            var need = perk.for;
+            if (current.level < need.level)
+                return false;
+
+            if (!_.isEmpty(need.classOnly)
+                && !_.contains(need.classOnly, current.id))
+                return false;
+
+            if (!_.isEmpty(need.classExcept)
+                && _.contains(need.classExcept, current.id))
+                return false;
+
+            return true;
         };
 
         $scope.addPerk = function (id) {
-            var perk = $scope.getPerk(id);
-            if (perk === undefined)
+            var perk = getPerk(id);
+            if (perk === undefined || $scope.currentStats === undefined)
                 return;
 
-            $scope.applyPerk(perk);
+            applyPerk(perk);
             perk.selected = !perk.selected;
         };
 
-        /////////////////////
 
-        $scope.applyPerk = function (perk) {
-            for (id in perk.effects) {
-                $scope.applyPerkEffect(id, perk.effects[id], perk.selected)
+        /* Private section */
+
+        function applyPerk(perk) {
+            _.each(perk.effects, function (stats, id) {
+                var applyEffect = effectsMap[id];
+                _.each(stats, function (value, id) {
+                    applyEffect(getCurrentStatWrapper(id), value, perk.selected);
+                })
+            })
+        }
+
+        var effectsMap = {
+                add: function (statWrapper, value, revert) {
+                    if (!_.isUndefined(statWrapper))
+                        statWrapper.set(statWrapper.get() + (revert ? -1 * value : value));
+                },
+                mul: function (statWrapper, value, revert) {
+                    if (!_.isUndefined(statWrapper))
+                        statWrapper.set(
+                            Math.round(statWrapper.get() * (revert ? 1 / value : value)));
+                },
+                provide: function (index, statId, revert) {
+                    if (revert)
+                        delete $scope.currentStats[statId];
+                    else
+                        $scope.currentStats[statId] = null;
+                }
             }
-        };
+            ;
 
-        $scope.applyPerkEffect = function (id, stats, revert) {
-            for (statId in stats) {
-                $scope.effectsMap[id](statId, stats[statId], revert);
-            }
-        };
-
-        $scope.effectsMap = {
-            add: function (stat, value, revert) {
-                if ($scope.currentStats[stat] !== undefined)
-                    $scope.currentStats[stat] += revert ? -1 * value : value;
-            }
-        };
-
-        $scope.isAncestor = function (ancestorId, child) {
-            if (ancestorId === undefined || child === undefined)
+        function isAncestor(ancestorId, child) {
+            if (_.isUndefined(ancestorId) || _.isUndefined(child))
                 return false;
             if (child.parent === ancestorId)
                 return true;
-            return $scope.isAncestor(ancestorId, $scope.getClass(child.parent));
-        };
+            return isAncestor(ancestorId, getClass(child.parent));
+        }
 
-        $scope.calculateStats = function (classId) {
-            var result = {};
-            if ($scope.data === undefined || classId === undefined)
-                return result;
+        function calculateStats(id, result) {
+            if (_.isUndefined(result))
+                result = {}
 
-            return $scope.recursiveCalculateStats(classId, {});
-        };
-
-        $scope.recursiveCalculateStats = function (id, result) {
-            var clazz = $scope.getClass(id);
-            if (clazz === undefined)
+            var clazz = getClass(id);
+            if (_.isUndefined(clazz))
                 return result;
 
             var stats = clazz.stats.base;
             for (var key in stats) {
-                if (result[key] === undefined)
+                if (!_.has(result, key))
                     result[key] = stats[key];
             }
             stats = clazz.stats.special;
             for (var key in stats) {
-                if (result[key] === undefined)
+                if (!_.has(result, key))
                     result[key] = stats[key];
+                else {
+                    // TODO make it recursive
+                    if (_.isObject(stats[key])) {
+                        for (var subKey in stats[key]) {
+                            if (!_.has(result[key], subKey)) {
+                                result[key][subKey] = stats[key][subKey];
+                            }
+                        }
+                    }
+                }
             }
-            return $scope.recursiveCalculateStats(clazz.parent, result);
-        };
-
-        $scope.getClass = function (id) {
-            return $scope.getJsonElement("classes", id);
-        };
-
-        $scope.getPerk = function (id) {
-            return $scope.getJsonElement("perks", id);
-        };
-
-        $scope.getJsonElement = function (container, id) {
-            var result = jsonPath($scope.data[container], "$.." + id);
-            return  result === undefined || result.length == 0 ? undefined : result[0];
+            return calculateStats(clazz.parent, result);
         }
-    });
+
+        // TODO make it recursive or else
+        function getCurrentStatWrapper(id) {
+            if (!_.isUndefined($scope.currentStats[id]))
+                return {
+                    get: function () {
+                        return $scope.currentStats[id]
+                    },
+                    set: function (v) {
+                        $scope.currentStats[id] = v
+                    }
+                };
+
+            for (var i in $scope.currentStats) {
+                var statData = $scope.currentStats[i];
+                if (_.isEmpty(statData))
+                    continue;
+
+                for (var subId in statData) {
+                    if (subId == id) {
+                        return {
+                            get: function () {
+                                return $scope.currentStats[i][subId]
+                            },
+                            set: function (v) {
+                                $scope.currentStats[i][subId] = v
+                            }
+                        }
+                    }
+                }
+            }
+
+            return undefined;
+        }
+
+        function getClass(id) {
+            return getDataJsonElement("classes", id);
+        }
+
+        function getPerk(id) {
+            return getDataJsonElement("perks", id);
+        }
+
+        function getDataJsonElement(container, id) {
+            return getJsonElement($scope.data[container], id);
+        }
+
+        function getJsonElement(json, id) {
+            var result = jsonPath(json, "$.." + id);
+            return  _.isEmpty(result) ? undefined : result[0];
+        }
+    }
+)
+;
+
+sofApp.factory('RecursionHelper', ['$compile', function ($compile) {
+    return {
+        /**
+         * Manually compiles the element, fixing the recursion loop.
+         * @param element
+         * @param [link] A post-link function, or an object with function(s) registered via pre and post properties.
+         * @returns An object containing the linking functions.
+         */
+        compile: function (element, link) {
+            // Normalize the link parameter
+            if (angular.isFunction(link)) {
+                link = { post: link };
+            }
+
+            // Break the recursion loop by removing the contents
+            var contents = element.contents().remove();
+            var compiledContents;
+            return {
+                pre: (link && link.pre) ? link.pre : null,
+                /**
+                 * Compiles and re-adds the contents
+                 */
+                post: function (scope, element) {
+                    // Compile the contents
+                    if (!compiledContents) {
+                        compiledContents = $compile(contents);
+                    }
+                    // Re-add the compiled contents to the element
+                    compiledContents(scope, function (clone) {
+                        element.append(clone);
+                    });
+
+                    // Call the post-linking function, if any
+                    if (link && link.post) {
+                        link.post.apply(null, arguments);
+                    }
+                }
+            };
+        }
+    };
+}]);
